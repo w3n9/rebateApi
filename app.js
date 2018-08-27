bc66c0d01ddb5f3a17493a502c98a9605510b5c5
StringTek <283240408@qq.com>
2018-08-27 22:42:02 +0800

    随便改改

--- app.js
+++ app.js
@@ -7,10 +7,20 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var app = express();
 var config=require('./config');
+var urlUtil=require('./util/urlUtil');
 var expressJwt=require('express-jwt');
+var axios=require('axios');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+
+//全局变量的定义
+global.access_token="123";
+global.fresh_token="";
+global.isPddAuthorized=false;
+
+
 
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -18,7 +28,16 @@ app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By",' 3.2.1')
     res.header("Content-Type", "application/json;charset=utf-8");
-    next();
+    if(global.isAuthorization===false){
+        let url = urlUtil.urlConcat("http://jinbao.pinduoduo.com/open.html",{
+            client_id:config.pdd_client_id,
+            response_type:code,
+            redirect_uri:config.pdd_redirect_uri,
+        });
+
+    }else{
+        next();
+    }
 });
 
 
@@ -29,7 +48,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use(expressJwt({secret: config.secret}).unless({path: ["/users/login","/users/register"]}));
+app.use(expressJwt({secret: config.secret}).unless({path: [
+        "/users/login",
+        "/users/register",
+        "/test","/favicon.ico",
+        "/is_pdd_authorized",
+        '/authorization_handle']}));
 app.use(function (err, req, res, next) {
     if (err.name === "UnauthorizedError") {
         res.status(401).send("invalid token");
--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+
 var config={
     "mysql":{//mysql配置
         host:"localhost",
@@ -6,10 +7,15 @@ var config={
         database:"rebate"
     },
     "secret":"ajdao1231231aosdjoasd",//jwt秘钥
+    "api_url":"http://localhost",//本应用的地址
     "pdd_api":"http://gw-api.pinduoduo.com/api/router",//拼多多api后台地址
     "pdd_client_id": "62b810aca7504bf19feb6ab3cbe71150",//拼多多应用id
     "pdd_client_secret": "a96f20eb3a9fa97fc7a8110cc293c4d29a42d7ef",//拼多多应用秘钥
-    "pdd_redirect_uri":"https://www.baidu.com",//拼多多应用回调地址（与拼多多开放平台中的应用需保持一致）
+    "pdd_redirect_uri":this.api_url+"/authorization_handle",//拼多多应用回调地址（与拼多多开放平台中的应用需保持一致）
+    "pdd_access_token_get_url":"http://open-api.pinduoduo.com/oauth/token",//拼多多获取access_token请求地址
+
+    // "pdd_access_token_dev":"",//拼多多开发环境access_token
+    // "pdd_refresh_token_dev":""//拼多多开发环境refresh_token
 };
 
 
