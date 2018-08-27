8dfba277353aca8030cf8a81c7c1e9eb56589a2b
StringTek <283240408@qq.com>
2018-08-27 10:48:48 +0800

    引入jwt以及配置文件以及跨域访问

--- app.js
+++ app.js
@@ -6,17 +6,38 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var app = express();
-
-
+var config=require('./config');
+var expressJwt=require('express-jwt');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(expressJwt({secret: config.secret}).unless({path: ["/users/login","/users/register"]}));
+app.use(function (err, req, res, next) {
+    if (err.name === "UnauthorizedError") {
+        res.status(401).send("invalid token");
+    }
+});
+
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
